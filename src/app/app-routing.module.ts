21c2c36267dc5bd0d355f8a992b0a3d7981dd2ee
Bezier.Ho <Bezier.Ho@testritegroup.com>
2018-12-12 10:31:48 +0800

    初始化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component'; //指定的component (這裡不用副檔名)
+import { HellobzComponent } from './hellobz/hellobz.component'
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/hellobz', pathMatch: 'full' }, // 這裡可以寫預設路由要指到哪裡，會自動轉跳
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'hellobz', component: HellobzComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+  // 詳細說明的Component, path 中的冒号（:）表示 :id 是一个占位符，它表示某个特定英雄的 id (深層路由)
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes)], //始化路由器
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
+// 這裡是整個服務最TOP的 也就是根模塊
+// NgModule 用于描述应用的各个部分如何组织在一起。 每个应用又至少一个 Angular 模块，根模块就是你用来启动此应用的模块。
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';// <-- NgModel lives here 真可憐 只好全域import NgModel 之下的模塊
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { HellobzComponent } from './hellobz/hellobz.component'
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HttpClientModule }    from '@angular/common/http'; // 好像是專門處理执行 HTTP 请求的方法
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
-@NgModule({
-  declarations: [
-    AppComponent
+
+
+@NgModule({ // 這也是一個裝飾器
+  declarations: [ //该应用所拥有的组件 依賴的模塊寫這裡 (這裡都是有View的)(網站裡面所有的都要寫在這喔
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    HellobzComponent,
+    HeroSearchComponent
   ],
-  imports: [
+  imports: [ // 匯入的 Module(angular自己寫的沒有 View的 Module) 在這裡 导入 BrowserModule 以获取浏览器特有的服务，比如 DOM 渲染、无害化处理和位置
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // 各种服务提供商 提用来指导 Dependency injection 系统 (依賴注入)该如何获取某个依赖的值( 資料 )。 大多数情况下，这些依赖就是你要创建和提供的那些服务，它们是全应用范围的。
+  bootstrap: [AppComponent] // 根组件，Angular 创建它并直接插入 index.html 宿主页面，註:插入的是該模塊的 Class 名稱
 })
 export class AppModule { }
+
+// 更多關於這個檔案請看 https://angular.cn/guide/bootstrapping
